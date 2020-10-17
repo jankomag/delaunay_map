7581e36e7522ecd6619765f614135c7a1d47ff77
Jan Magnuszewski <janekmagnuszewski@o2.pl>
2020-10-17 13:42:23 +0100

    adding continents points

--- map_design.R
+++ map_design.R
@@ -4,22 +4,23 @@ library(sf)
 library(nngeo)
 library(ggplot2)
 library(ggthemes)
- 
-pts = as.data.frame(read.csv('points2.csv', sep = ";"))
+
+pts = as.data.frame(read.csv('points/asi.csv', sep = ";"))
 
 
 #list of points to map
-oldpts <- data.frame(y = c( 33.4,34.7,38.5,39.7,41.7, 39.1, 30.4, 33.7),
-                  x = c( -112,-92.3,-121.4,-104.9,-72.6,-75.5, -84.2,-84.4))
+#oldpts <- data.frame(y = c( 33.4,34.7,38.5,39.7,41.7, 39.1, 30.4, 33.7),
+#                    x = c( -112,-92.3,-121.4,-104.9,-72.6,-75.5, -84.2,-84.4))
 #oldpts$id <- seq.int(nrow(pts))
 
 #convert to an sf object and project
 pts <- st_as_sf(pts, coords = c("X", "Y"), crs = 4326)
+
 pts <- st_transform(pts, 4326)
 
 #find the nearest n neighbors to each point
 neighbors <- 5
-nearest <- st_nn(pts, pts, k = neighbors+1, maxdist = 2100000) 
+nearest <- st_nn(pts, pts, k = neighbors+1, maxdist = 2500000) 
 
 #connect each point to its nearest neighbors
 connect <- st_connect(pts, pts, nearest) %>% st_as_sf
